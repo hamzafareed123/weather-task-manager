b7a93146c361262d38f3dcc86c005be74b218682
Hamza <hamzafareed616@gmail.com>
2026-02-27 16:45:05 +0500

    fix env issues

--- Backend/src/config/env.ts
+++ Backend/src/config/env.ts
@@ -1,10 +1,14 @@
 import dotenv from "dotenv";
 
-dotenv.config();
+
+if (process.env.NODE_ENV !== "test" && process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
 export const ENV = {
   PORT: parseInt(process.env.PORT || "8000", 10),
   MONGO_URL: process.env.MONGO_URL,
   JWT_SECRET: process.env.JWT_SECRET as string,
   CLIENT_URL: process.env.CLIENT_URL,
-  MONGO_TEST_URI:process.env.MONGO_TEST_URI as string
+  MONGO_TEST_URI: process.env.MONGO_TEST_URI as string
 };
